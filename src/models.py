ff1742b26a91f9e6078baf740b9ef9ef13d2b8f1
TheoYanvno <TheoYanvno@gmail.com>
2023-02-09 04:08:57 +0000

    inital push

--- src/models.py
+++ src/models.py
@@ -3,17 +3,50 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return '<User %r>' % self.email
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "password": self.password,
+            # do not serialize the password, its a security breach
+        }
+class Favorites(db.Model):
+    __tablename__ = 'Favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    USER_ID = db.Column(db.Integer, db.ForeignKey('User.id'))
+    FAV_CHARACTER_ID = db.Column(db.Integer, db.ForeignKey('Character.id'))
+    FAV_PLANET_ID = db.Column(db.Integer, db.ForeignKey("Planet.id"))
+    user = db.relationship("User", foreign_keys=[USER_ID])
+    character = db.relationship('Character')
+    planet = db.relationship('Planet')
+class Character(db.Model):
+    __tablename__ = 'Character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    description = db.Column(db.String(120))
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+            # do not serialize the password, its a security breach
+        }
+class Planet(db.Model):
+    __tablename__ = 'Planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    description = db.Column(db.String(120))
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
             # do not serialize the password, its a security breach
         }
